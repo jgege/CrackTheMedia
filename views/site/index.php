2b4dfa6f2ff277a5d7a52b14bb1f576c0620c35e
Ubuntu <ubuntu@ip-172-31-40-150.eu-west-1.compute.internal>
2017-03-19 09:50:44 +0000

    api description

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,52 @@
-<?php
-
-/* @var $this yii\web\View */
-
-$this->title = 'My Yii Application';
-?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+<style>
+    body{ margin: 0;
+        background-color: #212121;
+    }
+    .container { background-color: #212121;
+    }
+    .header { position: relative;
+        height: 128px;
+        background-color: #F5AB35;
+    }
+    .title_image { float: left;
+    }
+    .title_text { float: left;
+    }
+    .title_text h1 { font-size: 52px;
+    }
+    .content { margin-top: 10px;
+        padding: 64px;
+    }
+    .content h1 { color: white;
+    }
+    .code { display: inline-block;
+        margin: 16px;
+        padding: 16px;
+        border-style: none;
+        border-radius: 8px;
+        background-color: darkgray;
+        width: auto;
+    }
+</style>
+<div class="header">
+    <div class="title_image">
+        <img src="extension-icon.png" alt="title image"/>
     </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
+   <div class="title_text">
+       <h1>Crack the Media API</h1>
+   </div>
+</div>
+<div class="content">
+    <h1>Media outlet basic information</h1>
+    <div class="code">
+        GET http://crackthemedia.ml/boyan/api/index?query={URL_QUERY_HERE}
+    </div>
+    <h1>Image original source information</h1>
+    <div class="code">
+        GET http://crackthemedia.ml/boyan/api/image?query={URL_IMG_SRC_HERE}
+    </div>
+    <h1>Check article against other sources</h1>
+    <div class="code">
+        GET http://crackthemedia.ml/boyan/api/similarity?query={ARTICLE_NAME_HERE}
     </div>
 </div>
